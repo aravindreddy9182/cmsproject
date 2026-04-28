34c47c4491e16ee24b567efa1c4d6a790652a6af
aravindreddy9182 <aravindreddy97011@gmail.com>
2026-04-28 11:59:29 +0530

    Update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -8,13 +8,18 @@ from werkzeug.utils import secure_filename
 from flask import flash
 
 blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
+blob_service = BlockBlobService(
+    account_name=app.config['BLOB_ACCOUNT'],
+    account_key=app.config['BLOB_STORAGE_KEY']
+)
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -28,12 +33,15 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+
 class Post(db.Model):
     __tablename__ = 'posts'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
     author = db.Column(db.String(75))
@@ -51,18 +59,26 @@ class Post(db.Model):
         self.body = form.body.data
         self.user_id = userId
 
-        if file:
-            filename = secure_filename(file.filename);
-            fileextension = filename.rsplit('.',1)[1];
-            Randomfilename = id_generator();
-            filename = Randomfilename + '.' + fileextension;
-            try:
-                blob_service.create_blob_from_stream(blob_container, filename, file)
-                if(self.image_path):
-                    blob_service.delete_blob(blob_container, self.image_path)
-            except Exception:
-                flash(Exception)
-            self.image_path =  filename
+        if file and file.filename:
+            filename = secure_filename(file.filename)
+
+            if '.' in filename:
+                fileextension = filename.rsplit('.', 1)[1]
+                randomfilename = id_generator()
+                filename = randomfilename + '.' + fileextension
+
+                try:
+                    blob_service.create_blob_from_stream(blob_container, filename, file)
+
+                    if self.image_path:
+                        blob_service.delete_blob(blob_container, self.image_path)
+
+                    self.image_path = filename
+
+                except Exception as e:
+                    flash(str(e))
+
         if new:
             db.session.add(self)
+
         db.session.commit()
